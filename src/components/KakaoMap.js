d46d603f7d1d526d6594458d53fa21a1b433feda
kys0017 <kys0017@gmail.com>
2021-09-20 19:25:53 +0900

    clear git cache

--- src/components/KakaoMap.js
+++ src/components/KakaoMap.js
@@ -1,5 +1,5 @@
 /*global kakao*/ // <---- 주석인줄 알았는데 이거 업으면 kakao 객체 없다고 에러남
-import React, {useEffect, useRef, useState} from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const KakaoMap = () => {
     const [kakaoMap, setKakaoMap] = useState(null);
@@ -17,15 +17,21 @@ const KakaoMap = () => {
                 const center = new kakao.maps.LatLng(37.50802, 127.062835);
                 const options = {
                     center,
-                    level: 3
+                    level: 3,
                 };
                 const map = new kakao.maps.Map(container.current, options);
                 setKakaoMap(map);
             });
         };
-    }, [])
+    }, []);
 
-    return <div id="map" ref={container} style={{width: '100wv', height: '100vh'}}/>;
+    return (
+        <div
+            id="map"
+            ref={container}
+            style={{ width: '100wv', height: '100vh' }}
+        />
+    );
 };
 
 export default KakaoMap;
