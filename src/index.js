33c7c688121c6d5b1d05ce8aab74adcb3f9e3c47
kys0017 <kys0017@gmail.com>
2021-09-30 23:46:40 +0900

    :package: redux 개발자 툴 추가

--- src/index.js
+++ src/index.js
@@ -7,8 +7,10 @@ import reportWebVitals from './reportWebVitals';
 import { createStore } from 'redux';
 import rootReducer from './modules';
 import { Provider } from 'react-redux';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
+// composeWithDevTools 를 사용하여 리덕스 개발자 도구 활성화
 
 ReactDOM.render(
     <Provider store={store}>
