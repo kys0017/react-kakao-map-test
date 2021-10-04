83c1d2e34df63408ff38138fda28ab218cac6f37
kys0017 <kys0017@gmail.com>
2021-10-05 02:05:04 +0900

    :bento: 현재 위치 버튼 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import KakaoMap from './components/KakaoMap';
-import KakaoMapZoomControl from './components/KakaoMapZoomControl';
-import KakaoMapTypeControl from './components/KakaoMapTypeControl';
-import SearchBox from './components/SearchBox';
+import KakaoMapZoomControl from './components/map/KakaoMapZoomControl';
+import KakaoMapTypeControl from './components/map/KakaoMapTypeControl';
+import SearchBox from './components/search/SearchBox';
+import KakaoMapCurrentPosition from './components/map/KakaoMapCurrentPosition';
 
 function App() {
     return (
         <>
             <KakaoMap>
+                <KakaoMapCurrentPosition />
                 <KakaoMapTypeControl />
                 <KakaoMapZoomControl />
             </KakaoMap>
